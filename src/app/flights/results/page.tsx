b571b730ce73531f464200dadb5a98ca56fe0e40
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-12 15:51:18 +0600

    result page error solved

--- src/app/flights/results/page.tsx
+++ src/app/flights/results/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // /* eslint-disable @next/next/no-img-element */
 // /* eslint-disable @typescript-eslint/no-explicit-any */
 // /* eslint-disable @typescript-eslint/no-unused-vars */
